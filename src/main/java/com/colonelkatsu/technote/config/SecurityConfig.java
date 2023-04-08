9834101e10a0c435b096c0f61c8b3cad8ed01375
Jungman Berliansyah Nurdin <NSIDN-09007@nsidn.local>
2023-04-08 17:29:48 +0700

    Deleted authentications

--- src/main/java/com/colonelkatsu/technote/config/SecurityConfig.java
+++ src/main/java/com/colonelkatsu/technote/config/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.colonelkatsu.technote.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,14 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-  @Autowired
-  private UserDetailsService userDetailsService;
-
-//  @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-//  public AuthenticationManager authenticationManagerBean() throws Exception {
-//      return authenticationManagerBean();
-//  }
-
   @Bean
   SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.csrf().disable().authorizeHttpRequests()
@@ -32,10 +19,6 @@ public class SecurityConfig {
       .anyRequest().authenticated()
       .and().httpBasic();
     return httpSecurity.build();
-  }
-
-  public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
   }
 
   @Bean
