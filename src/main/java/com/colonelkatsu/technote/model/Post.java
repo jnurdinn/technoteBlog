ce00c5fb46b89234bf224e0c76a1d48e8d9587ef
Jungman Berliansyah Nurdin <NSIDN-09007@nsidn.local>
2023-04-08 21:15:45 +0700

    Updated post model

--- src/main/java/com/colonelkatsu/technote/model/Post.java
+++ src/main/java/com/colonelkatsu/technote/model/Post.java
@@ -1,6 +1,7 @@
 package com.colonelkatsu.technote.model;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Component;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,33 +15,21 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
-@Entity
-@Table
-@Component
 public class Post {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   private Long id;
 
-  @NotBlank
-  @Column
   private String title;
 
-  @Lob
-  @Column
-  @NotEmpty
-  private String content;
+  @Column(columnDefinition = "TEXT")
+  private String body;
 
-  @Column
-  private Instant createdOn;
+  private LocalDateTime createdAt;
 
-  @Column
-  private Instant upgradedOn;
-
-  @Column
-  @NotBlank
-  private String username;
+  private LocalDateTime updatedAt;
 }
