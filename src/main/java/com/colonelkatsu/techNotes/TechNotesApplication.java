0f2dd1ec6b80a2b008825300edde2e442ecd5edf
Jungman Berliansyah Nurdin <NSIDN-09007@nsidn.local>
2023-04-08 22:55:56 +0700

    Updated the fresh one

---
+++ src/main/java/com/colonelkatsu/techNotes/TechNotesApplication.java
@@ -0,0 +1,13 @@
+package com.colonelkatsu.techNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechNotesApplication.class, args);
+	}
+
+}
