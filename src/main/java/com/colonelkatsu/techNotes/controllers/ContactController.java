6b3ff316ea3928197acb4edded5a419fcd12db98
Jungman Nurdin <jungmannurdin@192.168.1.3>
2023-04-21 06:14:31 +0800

    delete unused import

--- src/main/java/com/colonelkatsu/techNotes/controllers/ContactController.java
+++ src/main/java/com/colonelkatsu/techNotes/controllers/ContactController.java
@@ -1,6 +1,5 @@
 package com.colonelkatsu.techNotes.controllers;
 
-import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
